9b06af6850b923f94f7762446de47415981e774a
sagar <146479284+sagar9285@users.noreply.github.com>
2024-04-13 18:46:05 +0000

    variable done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 221155
+let accountEmail = "sagargupta@gmail.com"
+var accountPassword = " 5623"
+accountCity = "jaipur"
+
+let accountState ; 
+
+// accountId = 54213   // not allowed
+
+/*
+we not prefer to var 
+instead of var we use let
+*/
+
+accountEmail = "sg1316761@gmail.com"
+accountPassword =" 0000"
+accountCity = " bhopal"
+
+console.table([ accountId , accountEmail ,accountPassword ,accountCity, accountState]);
